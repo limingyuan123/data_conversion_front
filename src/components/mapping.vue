1f9874a7b6a61f006c0e8b073a86bce082dd6b7a
limingyuan123 <1484156313@qq.com>
2020-10-21 14:33:37 +0800

    login & register & mapping

---
+++ src/components/mapping.vue
@@ -0,0 +1,43 @@
+<style>
+.schema{
+    margin-left: 2%;
+    width: 45%;
+    height: auto;
+    float: left;
+}
+.tran{
+    margin-right: 2%;
+    width: 45%;
+    height: auto;
+    float: right;
+}
+</style>
+<template>
+<div>
+<el-card class="schema">
+    schema
+
+</el-card>
+<el-card class="tran">
+    transform method:
+    
+</el-card>
+</div>
+</template>
+<script>
+export default {
+  name: "mapping",
+  data() {
+    return {
+      
+    };
+  },
+  computed: {},
+  methods: {
+    
+  },
+  mounted(){
+
+  }
+};
+</script>
