27d81ed3e71e4e026bc83bcf2e96eeb0dcd62431
limingyuan123 <1484156313@qq.com>
2020-11-09 10:28:36 +0800

    11.9

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -73,21 +73,24 @@ body {
         <el-menu-item index="3" @click="turnToTemplate"
           >Template</el-menu-item
         >
+        <el-menu-item index="4" @click="turnToHelp"
+          >Help</el-menu-item
+        >
         <!-- <el-submenu index="4">
           <template slot="title">My workspace</template>
           <el-menu-item index="4-1">option1</el-menu-item>
           <el-menu-item index="4-2">option2</el-menu-item>
           <el-menu-item index="4-3">option3</el-menu-item>
         </el-submenu> -->
-        <el-menu-item index="4"
+        <el-menu-item index="5"
           ><a href="http://opengmsteam.com/" target="_blank"
             >About us</a
           ></el-menu-item
         >
-        <el-menu-item index="5" style="margin-left: 30%;">
+        <el-menu-item index="6" style="margin-left: 25%;">
           <span slot="title" @click="turnToLogin">Login</span>
         </el-menu-item>
-        <el-menu-item index="6">
+        <el-menu-item index="7">
           <span slot="title" @click="turnToSign">Sign up</span>
         </el-menu-item>
       </el-menu>
@@ -143,6 +146,9 @@ export default {
     },
     turnToService(){
       this.$router.replace("/service");
+    },
+    turnToHelp(){
+      alert("hello im help sir");
     }
   }
 };
